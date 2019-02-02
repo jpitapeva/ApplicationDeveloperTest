c1fa5a3458e2b1ed1cab76134a6f1ee9e50ff680
Engenheiro Joao Antunes <joaopaulo.m@terra.com.br>
2019-02-02 20:39:16 -0200

    #Volvo - Test feat: add. front

---
+++ Volvo/Volvo/Models/Front.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Volvo.Models
+{
+    public class Front
+    {
+        [Required]
+        public int Id { get; set; }
+    }
+}
---
+++ Volvo/VolvoTest/ControllerTest.cs
@@ -0,0 +1,34 @@
+using System;
+using Volvo.Controllers;
+using Volvo.Controllers.v1;
+using Xunit;
+using TruckServices = Volvo.Services.Interface;
+
+namespace VolvoTest
+{
+    public class ControllerTest
+    {
+        private readonly Volvo.Services.TruckServices _iTruckServices;
+
+        public ControllerTest(Volvo.Services.TruckServices itruckServices)
+        {
+            _iTruckServices = itruckServices;
+        }
+
+        [Fact]
+        public void GetViewResultHomeController()
+        {
+            var controller = new HomeController();
+            var result = controller.About().ToString();
+            Assert.Equal("Microsoft.AspNetCore.Mvc.ViewResult", result);
+        }
+
+        [Fact]
+        public void GetTruckById()
+        {
+            var controller = new TruckController(_iTruckServices);
+            var result = controller.GetTruckById(new Guid("e4d330dc-f256-4a95-94b6-b10d045e65a9"));
+            Assert.Equal("Ok", result.ToString());
+        }
+    }
+}
